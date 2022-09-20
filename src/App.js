96fe8bf00d2fdb78509348e65d338d3c557178a3
Anvesha-gt <anveshagautam06@gmail.com>
2022-09-20 10:39:08 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+// import Login from './components/Login';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+// import NotFound from './components/NotFound';
+ import Header from './components/Header';
+// import Eventhandling from './components/Eventhandling';
+import Login from './components/Login';
+import Register from './components/Register';
+import Plugin from './components/Plugin';
+import Dashboard from './components/Dashboard';
+
+// import UserManager from './components/UserManager';
+//  import { Toaster } from 'react-hot-toast';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <BrowserRouter>
+        <Header />
+        {/* <Link to="/home">Home</Link>
+        <Link to="/login">Login</Link> */}
+        <Routes>
+
+          <Route element={<Home></Home>} path="/" />
+          <Route element={<Home></Home>} path="home" />
+          <Route element={<Login></Login>} path="Login" />
+          <Route element={<Register></Register>} path="Register" />
+          <Route element={<Authorize ></Authorize >} path="Authorize" />
+          <Route element={<Plugin></Plugin>} path="Plugin" />
+          <Route element={<Dashboard></Dashboard >} path="Dashboard" />
+
+
+
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
